7444689f4c49646b9df86734c748bb908ca877ac
Dylan Sleith <sleithdylan@gmail.com>
2020-10-25 07:44:32 +0000

    Add footer

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 				</ul>
 				<ul class="navbar-nav">
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Log in
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -65,7 +65,7 @@
 						</div>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Sign up
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -230,7 +230,7 @@
 		</div>
 	</div>
 	<!-- CTA -->
-	<div id="cta" class="py-5">
+	<div id="cta" class="pb-5">
 		<div class="container">
 			<div class="row">
 				<h4 class="col-md text-left text-primary mt-5">Ready to get started?</h4>
@@ -244,6 +244,90 @@
 			</div>
 		</div>
 	</div>
+	<!-- Footer -->
+	<footer>
+		<nav class="bg-white navbar-light border-top mt-5">
+			<div class="container py-5">
+				<div class="row">
+					<div class="col-md-4 col-sm-12">
+						<div class="d-flex flex-column">
+							<a class="navbar-brand mb-5" href="./index.php">
+								<span class="font-weight-bold">Close</span><span class="font-weight-light">Apart</span>
+							</a>
+						</div>
+					</div>
+					<div class="col-md col-sm-6">
+						<ul class="navbar-nav mb-5">
+							<li class="nav-item">
+								<a class="nav-link active disabled pt-0" href="#">Product</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#use-cases">Use Cases</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#help-centre">Help Centre</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#status">Status</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md col-sm-6">
+						<ul class="navbar-nav mb-5">
+							<li class="nav-item">
+								<a class="nav-link active disabled pt-0" href="#">Company</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="./index.php#features">Features</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#support">Contact Support</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#sitemap">Sitemap</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md col-sm-6">
+						<ul class="navbar-nav mb-5">
+							<li class="nav-item">
+								<a class="nav-link active disabled pt-0" href="#">Legal</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#terms">Terms & Conditions</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#privacy">Privacy Policy</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link py-2" href="#disclaimer">Disclaimer</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md">
+						<div class="d-flex flex-column">
+							<p class="text-dark">&copy; CloseApart</p>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="socials mb-3 d-flex justify-content-sm-start justify-content-md-between">
+							<a class="text-dark h4" href="#">
+								<i class='bx bxl-twitter'></i>
+							</a>
+							<a class="text-dark h4 mx-4" href="#">
+								<i class='bx bxl-facebook-square'></i>
+							</a>
+							<a class="text-dark h4" href="#">
+								<i class='bx bxl-instagram'></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</footer>
 	<!-- Scripts -->
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
