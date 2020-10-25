ab5a099fe367688cf9bb1319ef2bdd4709a0f9a6
Dylan Sleith <sleithdylan@gmail.com>
2020-10-25 13:50:17 +0000

    Add smooth scroll

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// Smooth Scroll
+//- ----------------------------------------------
+
+// Select all links with hashes
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+      }
+    }
+  });
