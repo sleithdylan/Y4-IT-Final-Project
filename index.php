69ef9b3c3be3a09312907cccc388f7babcd5d5ac
Dylan Sleith <sleithdylan@gmail.com>
2020-10-25 06:24:21 +0000

    Add testimonials section

--- index.php
+++ index.php
@@ -79,14 +79,14 @@
 		</div>
 	</nav>
 	<!-- Hero -->
-	<div id="#home" class="d-flex flex-column justify-content-around align-items-center py-5">
+	<div id="home" class="d-flex flex-column justify-content-around align-items-center py-5">
 		<div class="container">
 			<div class="col-md-1"></div>
 			<div class="row d-flex justify-content-center align-items-center py-5">
 				<div class="col-md-5">
 					<h1 class="display-1 text-left">CloseApart</h1>
 					<p class="text-left">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer congue neque eu consectetur congue.</p>
-					<button type="button" class="btn btn-primary text-capitalize">Sign Up</button>
+					<button type="button" class="btn btn-primary text-capitalize px-5">Sign Up</button>
 				</div>
 				<div class="col-md-5">
 					<img style="height: 425px" src="./assets/images/boy-on-computer.png" alt="boy on computer">
@@ -107,7 +107,7 @@
 		</div>
 		<div class="container">
 			<div class="row py-5">
-				<div class="col-md-3 col-sm-6 col-sm-6 my-4 d-flex flex-column">
+				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
 					<div class="icon icon-lg icon-shape icon-shape-danger shadow rounded-circle">
 						<i class='bx bxs-dashboard'></i>
 					</div>
@@ -116,7 +116,7 @@
 						<p>Cras sodales malesuada turpis, et cursus ipsum accumsan vel.</p>
 					</div>
 				</div>
-				<div class="col-md-3 col-sm-6 col-sm-6 my-4 d-flex flex-column">
+				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
 					<div class="icon icon-lg icon-shape icon-shape-success shadow rounded-circle">
 						<i class='bx bx-stats'></i>
 					</div>
@@ -125,7 +125,7 @@
 						<p>Cras sodales malesuada turpis, et cursus ipsum accumsan vel.</p>
 					</div>
 				</div>
-				<div class="col-md-3 col-sm-6 col-sm-6 my-4 d-flex flex-column">
+				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
 					<div class="icon icon-lg icon-shape icon-shape-info shadow rounded-circle">
 						<i class='bx bxs-game'></i>
 					</div>
@@ -134,7 +134,7 @@
 						<p>Cras sodales malesuada turpis, et cursus ipsum accumsan vel.</p>
 					</div>
 				</div>
-				<div class="col-md-3 col-sm-6 col-sm-6 my-4 d-flex flex-column">
+				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
 					<div class="icon icon-lg icon-shape icon-shape-warning shadow rounded-circle">
 						<i class='bx bxs-cloud-upload'></i>
 					</div>
@@ -146,12 +146,94 @@
 			</div>
 		</div>
 	</div>
-	<!-- Scripts -->
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-	<script src="./assets/js/argon-design-system.min.js"></script>
-	<script src="./assets/js/main.js"></script>
+	<!-- Testimonials -->
+	<div id="testimonials" class="py-5">
+		<div class="container d-flex flex-column justify-content-around align-items-center">
+			<div class="row">
+				<h6 class="col-md text-center text-primary mt-5">Testimonials</h6>
+			</div>
+			<div class="row">
+				<h4 class="col-md text-center">Proin aliquet, mauris eu tempor <br /> hendrerit, lorem ligul.</h4>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row py-5">
+				<div class="col-lg-3 col-md-4 col-sm-6 my-4 d-flex flex-column">
+					<div class="card">
+						<div class="card-body shadow py-5">
+							<div class="icon icon-lg text-primary">
+								<i class='bx bxs-quote-alt-left'></i>
+							</div>
+							<p class="card-text">Aenean fringilla velit libero, vitae viverra dui aliquet vel. Pellentesque pulvinar elementum ullamcorper.</p>
+							<div class="d-flex align-items-center">
+								<img class="rounded-circle" style="width: 17.5%;" src="./assets/images/faces/sarah.jpg" alt="picture of sarah">
+								<div class="d-flex flex-column ml-3 mt-3">
+									<h6>Sarah</h6>
+									<p>Teacher</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 my-4 d-flex flex-column">
+					<div class="card">
+						<div class="card-body shadow py-5">
+							<div class="icon icon-lg text-primary">
+								<i class='bx bxs-quote-alt-left'></i>
+							</div>
+							<p class="card-text">Aenean fringilla velit libero, vitae viverra dui aliquet vel. Pellentesque pulvinar elementum ullamcorper.</p>
+							<div class="d-flex align-items-center">
+								<img class="rounded-circle" style="width: 17.5%;" src="./assets/images/faces/david.jpg" alt="picture of david">
+								<div class="d-flex flex-column ml-3 mt-3">
+									<h6>David</h6>
+									<p>Father</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 my-4 d-flex flex-column">
+					<div class="card">
+						<div class="card-body shadow py-5">
+							<div class="icon icon-lg text-primary">
+								<i class='bx bxs-quote-alt-left'></i>
+							</div>
+							<p class="card-text">Aenean fringilla velit libero, vitae viverra dui aliquet vel. Pellentesque pulvinar elementum ullamcorper.</p>
+							<div class="d-flex align-items-center">
+								<img class="rounded-circle" style="width: 17.5%;" src="./assets/images/faces/john.jpg" alt="picture of john">
+								<div class="d-flex flex-column ml-3 mt-3">
+									<h6>John</h6>
+									<p>Teacher</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 my-4 d-flex flex-column">
+					<div class="card">
+						<div class="card-body shadow py-5">
+							<div class="icon icon-lg text-primary">
+								<i class='bx bxs-quote-alt-left'></i>
+							</div>
+							<p class="card-text">Aenean fringilla velit libero, vitae viverra dui aliquet vel. Pellentesque pulvinar elementum ullamcorper.</p>
+							<div class="d-flex align-items-center">
+								<img class="rounded-circle" style="width: 17.5%;" src="./assets/images/faces/rebecca.jpg" alt="picture of rebecca">
+								<div class="d-flex flex-column ml-3 mt-3">
+									<h6>Rebecca</h6>
+									<p>Mother</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Scripts -->
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+		<script src="./assets/js/argon-design-system.min.js"></script>
+		<script src="./assets/js/main.js"></script>
 </body>
 
 </html>
