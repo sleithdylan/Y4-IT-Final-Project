48598441376fee84a65ab235c307ee4eb01159b9
Dylan Sleith <sleithdylan@gmail.com>
2021-03-06 17:28:05 +0000

    Add legal pages

---
+++ privacy.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Basic Page Needs -->
+	<title>CloseApart | Student’s online second home – participate in quizzes, communicate with teachers, complete your work online! Get comfortable with us</title>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="description"
+		content="Student’s online second home – participate in quizzes, communicate with teachers, complete your work online! Get comfortable with us">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./assets/images/favicon.ico" type="image/x-icon">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet">
+
+	<!-- Icons -->
+	<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="assets/css/argon-design-system.min.css">
+	<link rel="stylesheet" href="assets/css/argon-design-system-extras.min.css">
+</head>
+
+<body>
+	<!-- Navigation -->
+	<?php include('includes/navs/nav.php'); ?>
+	<!-- Hero -->
+	<div id="terms">
+		<div class="container">
+			<div class="row py-5">
+				<div class="col">
+					<h2><strong>Privacy Policy for CloseApart</strong></h2>
+
+					<p>At CloseApart, accessible from https://closeapart.co/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by CloseApart and how we use it.</p>
+
+					<p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+
+					<br>
+
+					<h3><strong>Log Files</strong></h3>
+
+					<p>CloseApart follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information. Our Privacy Policy was created with the help of the <a href="https://www.privacypolicyonline.com/privacy-policy-generator/">Privacy Policy Generator</a> and the <a href="https://www.generateprivacypolicy.com">Privacy Policy Generator</a>.</p>
+					
+					<br>
+
+					<h3><strong>Cookies and Web Beacons</strong></h3>
+
+					<p>Like any other website, CloseApart uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
+
+					<p>For more general information on cookies, please read the "What Are Cookies" article on <a href="https://www.privacypolicyonline.com/what-are-cookies/">Cookie Consent website</a>.</p>
+
+					<br>
+
+					<h3><strong>Privacy Policies</strong></h3>
+
+					<P>You may consult this list to find the Privacy Policy for each of the advertising partners of CloseApart.</p>
+
+					<p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on CloseApart, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+
+					<p>Note that CloseApart has no access to or control over these cookies that are used by third-party advertisers.</p>
+					
+					<br>
+
+					<h3><strong>Third Party Privacy Policies</strong></h3>
+
+					<p>CloseApart's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+
+					<p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?</p>
+					
+					<br>
+
+					<h3><strong>Children's Information</strong></h3>
+
+					<p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+
+					<p>CloseApart does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+					
+					<br>
+
+					<h3><strong>Online Privacy Policy Only</strong></h3>
+
+					<p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in CloseApart. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+					
+					<br>
+
+					<h3><strong>Consent</strong></h3>
+
+					<p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer -->
+	<?php include('includes/footers/footer_main.php'); ?>
+	<!-- Scripts -->
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script src="./assets/js/argon-design-system.min.js"></script>
+	<script src="./assets/js/main.js"></script>
+</body>
+
+</html>
