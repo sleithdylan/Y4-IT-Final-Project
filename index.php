168486ef54d47b1f21976cef9beb0280d99e8644
Dylan Sleith <sleithdylan@gmail.com>
2021-03-12 21:22:35 +0000

    Add GSAP & AOS animations

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
 	<link rel="stylesheet" href="assets/css/argon-design-system-extras.min.css">
 	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
 	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
 <body>
@@ -34,16 +35,18 @@
 	<div id="home" class="py-5">
 		<div class="container">
 			<div class="row d-flex flex-md-column flex-lg-row justify-content-center align-items-center py-5">
-				<div class="col-md-8 col-lg-5">
-					<h1 class="display-1 text-left">CloseApart</h1>
-					<p class="text-left">Student’s online second home – participate in quizzes, communicate with teachers,
+				<div class="col-md-8 col-lg-6">
+					<h1 class="display-1 text-left stagger-brand">CloseApart</h1>
+					<p class="text-left stagger-sub">Student’s online second home – participate in quizzes, communicate with teachers,
 						complete your work online!</p>
-					<p>Sign up as a:</p>
-					<a href="./student/signup.php" class="btn btn-primary text-capitalize px-5">Student</a>
-					<a href="./staff/signup.php" class="btn btn-outline-primary text-capitalize px-5">Staff</a>
+					<div class="stagger-cta">
+						<p>Sign up as a:</p>
+						<a href="./student/signup.php" class="btn btn-primary text-capitalize px-5">Student</a>
+						<a href="./staff/signup.php" class="btn btn-outline-primary text-capitalize px-5">Staff</a>
+					</div>
 				</div>
 				<div class="col-md-8 col-lg-6">
-					<img class="d-none d-sm-none d-md-block mt-md-5 mt-lg-0" style="height: 425px"
+					<img class="d-none d-sm-none d-md-block mt-md-5 mt-lg-0 stagger-image" style="height: 425px"
 						src="./assets/images/illustrations/closeapart-work.png" alt="closeapart work">
 				</div>
 			</div>
@@ -52,11 +55,11 @@
 	<!-- Who we are -->
 	<div id="who-we-are" class="py-5">
 		<div class="container">
-			<div class="row d-flex flex-md-column-reverse flex-lg-row justify-content-center align-items-center py-5 drone-image">
+			<div class="row d-flex flex-md-column-reverse flex-lg-row justify-content-center align-items-center py-5 drone-image" data-aos="fade-right">
 				<img class="d-none d-sm-none d-md-none d-lg-block shadow" style="height: 500px; width: 425px"
 						src="./assets/images/drone-school.png" alt="drone image of a school">
 				<div class="col-md col-lg"></div>
-				<div class="col-md col-lg-8 bg-white p-4 drone-text">
+				<div class="col-md col-lg-8 bg-white p-4 drone-text" data-aos="fade-left">
 					<h6 class="text-left text-primary mt-3">Who we are</h6>
 					<p class="text-left"><strong>CloseApart</strong> is a student portal driven by students. Our team’s goal is to
 						help out students complete their work online when coming into school is simply not possible. With the
@@ -72,48 +75,48 @@
 	<div id="features" class="py-5">
 		<div class="container">
 			<div class="row">
-				<h6 class="col-md text-left text-primary mt-5">Features</h6>
+				<h6 class="col-md text-left text-primary mt-5" data-aos="fade-up">Features</h6>
 			</div>
 			<div class="row">
-				<h4 class="col-md-5 text-left">Here's what you can expect</h4>
+				<h4 class="col-md-5 text-left" data-aos="fade-up" data-aos-duration="1000">Here's what you can expect</h4>
 			</div>
 		</div>
 		<div class="container">
 			<div class="row py-5">
 				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
-					<div class="icon icon-lg icon-shape icon-shape-danger shadow rounded-circle">
+					<div class="icon icon-lg icon-shape icon-shape-danger shadow rounded-circle" data-aos="zoom-in-up">
 						<i class='bx bxs-dashboard'></i>
 					</div>
 					<div class="pt-4">
-						<h6>Portal</h6>
-						<p>Get comfortable with us. Customise your profile</p>
+						<h6 data-aos="fade-up" data-aos-duration="1000">Portal</h6>
+						<p data-aos="fade-up" data-aos-duration="1000">Get comfortable with us. Customise your profile</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
-					<div class="icon icon-lg icon-shape icon-shape-success shadow rounded-circle">
+					<div class="icon icon-lg icon-shape icon-shape-success shadow rounded-circle" data-aos="zoom-in-up">
 						<i class='bx bx-stats'></i>
 					</div>
 					<div class="pt-4">
-						<h6>Communicate</h6>
-						<p>Share your thoughts. Upload informations for everyone</p>
+						<h6 data-aos="fade-up" data-aos-duration="1000">Communicate</h6>
+						<p data-aos="fade-up" data-aos-duration="1000">Share your thoughts. Upload informations for everyone</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
-					<div class="icon icon-lg icon-shape icon-shape-info shadow rounded-circle">
+					<div class="icon icon-lg icon-shape icon-shape-info shadow rounded-circle" data-aos="zoom-in-up">
 						<i class='bx bxs-game'></i>
 					</div>
 					<div class="pt-4">
-						<h6>Quizzes</h6>
-						<p>Take part in online quizzes and earn rewards</p>
+						<h6 data-aos="fade-up" data-aos-duration="1000">Quizzes</h6>
+						<p data-aos="fade-up" data-aos-duration="1000">Take part in online quizzes and earn rewards</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 my-4 d-flex flex-column">
-					<div class="icon icon-lg icon-shape icon-shape-warning shadow rounded-circle">
+					<div class="icon icon-lg icon-shape icon-shape-warning shadow rounded-circle" data-aos="zoom-in-up">
 						<i class='bx bxs-cloud-upload'></i>
 					</div>
 					<div class="pt-4">
-						<h6>Upload media</h6>
-						<p>Upload your work, share it with your teacher or class</p>
+						<h6 data-aos="fade-up" data-aos-duration="1000">Upload media</h6>
+						<p data-aos="fade-up" data-aos-duration="1000">Upload your work, share it with your teacher or class</p>
 					</div>
 				</div>
 			</div>
@@ -123,13 +126,13 @@
 	<div id="testimonials" class="py-5">
 		<div class="container">
 			<div class="row">
-				<h6 class="col-md text-left text-sm-center text-primary mt-5">Testimonials</h6>
+				<h6 class="col-md text-left text-sm-center text-primary mt-5" data-aos="fade-up">Testimonials</h6>
 			</div>
 			<div class="row">
-				<h4 class="col-md text-left text-sm-center">What do you think of us?<br />Let us know!</h4>
+				<h4 class="col-md text-left text-sm-center" data-aos="fade-up">What do you think of us?<br />Let us know!</h4>
 			</div>
 		</div>
-		<div class="container-fluid">
+		<div class="container-fluid" data-aos="fade-up">
 			<div class="row py-5 owl-carousel">
 				<div class="col-lg-12 col-md-12 col-sm-12 my-4">
 					<div class="card">
@@ -214,11 +217,11 @@
 	<div id="cta" class="pb-5 mb-5">
 		<div class="container">
 			<div class="row">
-				<h4 class="col-md text-left text-primary mt-5">Got any questions?</h4>
+				<h4 class="col-md text-left text-primary mt-5" data-aos="fade-up">Got any questions?</h4>
 			</div>
 			<div class="row">
-				<h4 class="col-md text-left">Contact us directly or visit the help centre</h4>
-				<div class="col-md-7 d-flex justify-content-sm-start justify-content-md-end align-items-center">
+				<h4 class="col-md text-left" data-aos="fade-up" data-aos-duration="1000">Contact us directly or visit the help centre</h4>
+				<div class="col-md-7 d-flex justify-content-sm-start justify-content-md-end align-items-center" data-aos="fade-up" data-aos-duration="1000">
 					<a href="./contact.php" class="btn btn-outline-primary text-capitalize px-4">Contact Us</a>
 					<a href="#help-centre" class="btn btn-outline-primary text-capitalize px-4">Help Centre</a>
 				</div>
@@ -233,7 +236,9 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<script src="./assets/js/argon-design-system.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
 	<script src="./assets/js/main.js"></script>
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 	<script>
 		// Carousel
 		$(document).ready(function () {
@@ -260,6 +265,14 @@
 					},
 				},
 			});
+		});
+		
+		// AOS
+		AOS.init({
+		  easing: 'ease-in-out',
+		  once: true,
+		  duration: 1000,
+			offset: 190
 		});
 	</script>
 </body>
